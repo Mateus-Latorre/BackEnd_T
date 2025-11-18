1d7ffeab8bf1d82f5822a597c4761fd56df19a45
Mateus Latorre <mateuslatorre220@gmail.com>
2025-11-18 09:08:23 -0300

    Criada a pasta Exercicio1

---
+++ POO/Pilares/Interface/Exercicios/Exercicio1/Circulo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Circulo : IForma
+    {
+        public void CalcularArea()
+        {
+            double r;
+            double a;
+            Console.WriteLine($"Digite o raio do círculo:");
+            r = double.Parse(Console.ReadLine());
+            a = r * r * Math.PI;
+            Console.WriteLine($"A área do círculo é de {a}");
+        }
+    }
+}
---
+++ POO/Pilares/Interface/Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Exercicio1;
+Console.Clear();
+Circulo c1 = new Circulo();
+c1.CalcularArea();
+Retangulo r1 = new Retangulo();
+r1.CalcularArea();
